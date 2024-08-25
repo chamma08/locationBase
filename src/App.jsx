c4c4603c65a046f71379a11348b2a475fe40bb2e
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2024-08-25 08:25:42 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>WebXR Location-Based App</h1>
+      <h1>Location Based App</h1>
       <LocationBasedModel modelUrl="/models/model2.gltf" />
     </div>
   );
